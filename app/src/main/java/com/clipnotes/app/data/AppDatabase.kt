df9520f074c4ae5e657ebe5c2affe70fffb7ed29
wmuuuue <daiiequwwe@outlook.com>
2025-11-25 13:13:58 +0800

    Update AppDatabase.kt

--- app/src/main/java/com/clipnotes/app/data/AppDatabase.kt
+++ app/src/main/java/com/clipnotes/app/data/AppDatabase.kt
@@ -8,7 +8,7 @@ import androidx.room.TypeConverters
 
 @Database(
     entities = [NoteEntity::class, PairedDeviceEntity::class],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
 @TypeConverters(Converters::class)
